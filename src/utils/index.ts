55f6abd3bb854fd7314d19cca860aa01b2f45075
Muhamad D. R. <muhammaddeni90@gmail.com>
2025-12-30 21:45:54 +0700

    feat: rearrange category

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
 export const menuItems = [
   { label: "Semua", href: "/" },
+  { label: "Kiat", href: "/tags/kiat" },
   { label: "Gagasan", href: "/tags/gagasan" },
-  { label: "Pustaka", href: "/tags/pustaka" },
   { label: "Rupa & Rasa", href: "/tags/rupa-rasa" },
+  { label: "Pustaka", href: "/tags/pustaka" },
   { label: "Perkakas", href: "/tags/perkakas" },
-  { label: "Lain-lain", href: "/tags/lain-lain" },
 ];
