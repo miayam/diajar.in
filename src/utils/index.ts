2557a0b8f3195f8076c5b5c2570699eb72aa5913
Muhamad D. R. <muhammaddeni90@gmail.com>
2026-01-01 20:35:04 +0700

    feat: add shimmer on landing page

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
 export const menuItems = [
   { label: "Semua", href: "/" },
-  { label: "Kiat", href: "/tags/kiat" },
   { label: "Gagasan", href: "/tags/gagasan" },
-  { label: "Rupa & Rasa", href: "/tags/rupa-rasa" },
   { label: "Pustaka", href: "/tags/pustaka" },
+  { label: "Rupa & Rasa", href: "/tags/rupa-rasa" },
+  { label: "Kiat", href: "/tags/kiat" },
   { label: "Perkakas", href: "/tags/perkakas" },
 ];
