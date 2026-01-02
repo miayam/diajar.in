e9a5f4777806ad9fa0a3920ea88e76ab047029b7
Muhamad D. R. <muhammaddeni90@gmail.com>
2026-01-02 07:36:56 +0700

    fix: don't use pure pitch black

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,31 @@ export const menuItems = [
   { label: "Kiat", href: "/tags/kiat" },
   { label: "Perkakas", href: "/tags/perkakas" },
 ];
+
+interface Slide {
+  text: string;
+}
+
+export const slides: Slide[] = [
+  {
+    text: "Bersihkan hati dan luruskan niat",
+  },
+  {
+    text: "Kurangi distraksi dan berikan fokus penuh",
+  },
+  {
+    text: "Jangan tinggi hati dengan apa yang dipelajari",
+  },
+  {
+    text: "Hindari perdebatan yang tidak perlu",
+  },
+  {
+    text: "Belajarlah secara menyeluruh",
+  },
+  {
+    text: "Prioritaskan pada ilmu yang bermuara pada kebijaksanaan hakiki",
+  },
+  {
+    text: "Hiasi ilmu dengan akhlak terpuji",
+  },
+];
