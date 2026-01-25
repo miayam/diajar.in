273985fe2e1a0e0ce813d4b900dd2710ccd12f0b
Muhamad D. R. <muhammaddeni90@gmail.com>
2026-01-25 17:14:04 +0700

    feat: provide sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeExternalLinks from "rehype-external-links";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import { loadEnv } from "vite";
 
 const { SITE } = loadEnv(process?.env?.NODE_ENV || "", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS(), mdx()],
+  integrations: [UnoCSS(), mdx(), sitemap()],
   prefetch: {
     prefetchAll: true,
   },
